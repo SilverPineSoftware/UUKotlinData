f0143d5fc5c2835a25a6b18accf8fc7a829a8688
Ryan DeVore <ryan@silverpine.com>
2025-10-25 19:49:29 -0700

    initial commit

---
+++ library/src/main/java/com/silverpine/uu/data/UUData.kt
@@ -0,0 +1,9 @@
+package com.silverpine.uu.data
+
+object UUData
+{
+    fun testMethod()
+    {
+        println("stub method to get library up and running")
+    }
+}
---
+++ library/src/main/java/com/silverpine/uu/data/converters/UUIntSetConverter.kt
@@ -0,0 +1,24 @@
+package com.silverpine.uu.data.converters
+
+import android.text.TextUtils
+import androidx.room.TypeConverter
+import kotlin.collections.mapNotNull
+import kotlin.collections.toSet
+import kotlin.text.toInt
+
+class UUIntSetConverter
+{
+    @TypeConverter
+    fun stringToObject(value: String?): Set<Int>?
+    {
+        val actualValue = value ?: return null
+        return TextUtils.split(actualValue, ",").mapNotNull { it.toInt() }.toSet()
+    }
+
+    @TypeConverter
+    fun objectToString(value: Set<Int>?): String?
+    {
+        val actualValue = value ?: return null
+        return TextUtils.join(",", actualValue)
+    }
+}
