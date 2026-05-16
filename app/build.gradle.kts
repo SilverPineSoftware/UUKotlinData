5f2c0d6abb87949984d4eb71efa2b220b1951d73
Ryan DeVore <ryan@silverpine.com>
2026-05-15 20:15:45 -0700

    update depends

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(uuBuild.plugins.uu.library.app)
+    alias(uuBuild.plugins.kotlin.serialization)
+    alias(uuBuild.plugins.kotlin.compose)
 }
 
 android {
@@ -10,16 +12,24 @@ android {
 }
 
 dependencies {
-    implementation(uuBuild.androidx.appcompat)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3.android)
-    implementation(libs.androidx.compose.runtime.android)
-    implementation(libs.androidx.compose.ui.tooling.preview.android)
+    implementation(libs.androidx.annotation.experimental)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.core.ktx)
     implementation(libs.google.material)
+    implementation(uuBuild.kotlinx.serialization.json)
     implementation(libs.uu.core.ktx)
     implementation(project(":library"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.runtime.android)
+    implementation(libs.androidx.compose.foundation.layout.android)
+    implementation(libs.androidx.compose.material3.android)
 
     androidTestImplementation(uuBuild.androidx.junit)
     androidTestImplementation(uuBuild.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,10 @@ plugins {
     alias(uuBuild.plugins.kotlin.android) apply false
     alias(uuBuild.plugins.nexus.publish)
     alias(uuBuild.plugins.kotlin.serialization)
+    alias(uuBuild.plugins.kotlin.compose) apply false
     alias(uuBuild.plugins.uu.library) apply false
     alias(uuBuild.plugins.uu.library.app) apply false
     alias(uuBuild.plugins.uu.android.test) apply false
     alias(uuBuild.plugins.uu.publish)
+
 }
