19ddc34a9156ec4cdb1752438b921327c7fcdc09
Ryan DeVore <ryan@silverpine.com>
2026-05-15 21:07:29 -0700

    refactor vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-extra["uu_namespace"] = "com.silverpine.uu.data"
-extra["uu_publish_artifact_id"] = "uu-data-ktx"
-extra["uu_publish_description"] = "Useful Utilities Data"
-extra["uu_scm_module_name"] = "UUKotlinData"
-
-extra["uu_min_sdk"] = 26
-extra["uu_target_sdk"] = 36
-extra["uu_java_version"] = 17
-
 plugins {
     alias(uuBuild.plugins.android.application) apply false
     alias(uuBuild.plugins.android.library) apply false
@@ -18,5 +9,4 @@ plugins {
     alias(uuBuild.plugins.uu.library.app) apply false
     alias(uuBuild.plugins.uu.android.test) apply false
     alias(uuBuild.plugins.uu.publish)
-
 }
